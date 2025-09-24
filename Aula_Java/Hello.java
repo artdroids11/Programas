f46252f7d50ba79967b7e92c79998dc60ad43675
artdroids11 <artdroidsmarcondes11@gmail.com>
2025-09-24 19:55:54 +0000

    Initial commit

--- Aula_Java/Hello.java
+++ Aula_Java/Hello.java
@@ -2,9 +2,21 @@ public class Hello {
     // Duas barra indica um comentário de uma linha
    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
- /* Barra e asterisco indicam um comentário de múltiplas linhas
-       Este é um exemplo de comentário */
 
+        // variaveis -> tipo: Textos (Strings)
+        // variaveis -> tipo: Numeros (Int)
+        // variaveis -> tipo: Numeros fracionados (Double)
+        // variaveis -> tipo: Booleans (verdadeiro | falso)
+
+        var login = "Very special" ;
+        double dec = 34.598898;
+        System.out.println(login);
+        System.out.println(dec);
+        
     }
 }   
+
+
+
+ /* Barra e asterisco indicam um comentário de múltiplas linhas
+       Este é um exemplo de comentário*/
